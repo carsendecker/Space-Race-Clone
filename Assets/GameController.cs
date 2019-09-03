a2cc004018165df712b7bd5c8eade3f44683e576
Carsen Decker <cad631@nyu.edu>
2019-09-03 16:56:58 -0400

    Started game, timer bar finished

---
+++ Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Slider timeSlider;
+    public float timeLimit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSlider.maxValue = timeLimit;
+        timeSlider.value = timeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimit -= Time.deltaTime;
+        timeSlider.value = timeLimit;
+    }
+}
