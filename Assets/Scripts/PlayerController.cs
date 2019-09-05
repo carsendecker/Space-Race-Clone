42e9990d7418a28d8d53a62e23b9a9c8da4a22ac
Carsen Decker <cad631@nyu.edu>
2019-09-05 15:21:09 -0400

    Star obstacles and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+    public KeyCode UpKey, DownKey;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(UpKey))
+        {
+            Vector2 pos = transform.position;
+            pos.y += MoveSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else if (Input.GetKey(DownKey))
+        {
+            Vector2 pos = transform.position;
+            pos.y -= MoveSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/StarMove.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarMove : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float WrapPosition;
+
+    private float wrapTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x += MoveSpeed * Time.deltaTime;
+        transform.position = pos;
+
+        if (Math.Abs(transform.position.x - WrapPosition) < 0.05)
+        {
+            pos.x = -WrapPosition;
+            transform.position = pos;
+        }
+    }
+}
