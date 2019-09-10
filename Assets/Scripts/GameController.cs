e719e25d5dd3bf3f813be18a4ceb3b3d0ce3193e
Carsen Decker <cad631@nyu.edu>
2019-09-10 03:16:31 -0400

    Spawning in crates and bombs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,15 +11,28 @@ public class GameController : MonoBehaviour
     public float timeLimit;
     public float playerStartY;
     public int player1Score, player2Score;
+    public float supplySpawnSpeed;
+    public int startingCrates;
+    public int bombChance;
 
     public TMP_Text p1ScoreText, p2ScoreText;
     public TMP_Text endText;
+    public GameObject supplyCrate;
+    public GameObject bombPickup;
+    public Vector2 supplySpawnBounds;
     
     // Start is called before the first frame update
     void Start()
     {
         timeSlider.maxValue = timeLimit;
         timeSlider.value = timeLimit;
+
+        for (int i = 0; i < startingCrates; i++)
+        {
+            SpawnSupplies();
+        }
+
+        StartCoroutine(SupplyTimer());
     }
 
     // Update is called once per frame
@@ -41,6 +54,51 @@ public class GameController : MonoBehaviour
         }
     }
 
+    //Spawns supply crates for both player's at the same time. Loops until the game stops
+    private IEnumerator SupplyTimer()
+    {
+        yield return new WaitForSeconds(supplySpawnSpeed);
+        SpawnSupplies();
+        StartCoroutine(SupplyTimer());
+    }
+
+    private void SpawnSupplies()
+    {
+        int chance = Random.Range(0, bombChance);
+        
+        //Spawn for player 1
+        //Random position within the bounds of player's play area
+        float randPosX = Random.Range(-supplySpawnBounds.x, -0.5f);
+        float randPosY = Random.Range(-supplySpawnBounds.y, supplySpawnBounds.y);
+
+        Vector2 randPos = new Vector2(randPosX, randPosY);
+        
+        if (chance != bombChance)
+        {
+            Instantiate(supplyCrate, randPos, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(bombPickup, randPos, Quaternion.identity);
+        }
+
+        //Spawn for player 2
+        randPosX = Random.Range(0.5f, supplySpawnBounds.x);
+        randPosY = Random.Range(-supplySpawnBounds.y, supplySpawnBounds.y);
+
+        randPos = new Vector2(randPosX, randPosY);
+        
+        if (chance != bombChance)
+        {
+            Instantiate(supplyCrate, randPos, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(bombPickup, randPos, Quaternion.identity);
+        }
+    }
+    
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player 1") || other.CompareTag("Player 2"))
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MoveSpeed;
-    public KeyCode UpKey, DownKey;
+    public int PlayerNumber;
+    public float MaxSpeed;
+    public float TurnSpeed;
+
+    public GameObject PickupParticles, StunnedParticles, DeathParticles;
 
     private Vector2 startPos;
+    private Rigidbody2D rb;
     
     void Start()
     {
         startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
     void Update()
     {
-        if (Input.GetKey(UpKey))
+        
+    }
+
+    private void Move()
+    {
+        float forwardSpeed = 0;
+        float rotateSpeed = 0;
+        if (PlayerNumber == 1)
         {
-            Vector2 pos = transform.position;
-            pos.y += MoveSpeed * Time.deltaTime;
-            transform.position = pos;
+            forwardSpeed = Input.GetAxis("Vertical") * MaxSpeed;
+            rotateSpeed = Input.GetAxis("Horizontal") * TurnSpeed;
         }
-        else if (Input.GetKey(DownKey))
+        else if (PlayerNumber == 2)
         {
-            Vector2 pos = transform.position;
-            pos.y -= MoveSpeed * Time.deltaTime;
-            transform.position = pos;
+            forwardSpeed = Input.GetAxis("Vertical2") * MaxSpeed;
+            rotateSpeed = Input.GetAxis("Horizontal2") * TurnSpeed;
         }
+
+        rb.velocity = transform.up * (forwardSpeed * Time.deltaTime);
+        transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -35,6 +53,10 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Star"))
         {
             StartCoroutine(DeathDelay());
+        }
+        else if (other.CompareTag("Supply"))
+        {
+            Destroy(other.gameObject);
         }
     }
 
@@ -46,6 +68,6 @@ public class PlayerController : MonoBehaviour
         transform.position = goAway;
         yield return new WaitForSecondsRealtime(1);
         transform.position = startPos;
-
+        transform.rotation = Quaternion.identity;
     }
 }
